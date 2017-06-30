b0b82b0fed4f0852e0fdf144de59efb2c0646316
ParkerWSmith <smithparker0@gmail.com>
2017-06-30 14:51:23 -0500

    Add project files.

---
+++ DiscordApp_v.02/SquidKid.cs
@@ -0,0 +1,50 @@
+﻿using Discord;
+using Discord.Commands;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordApp_v._02
+{
+    class SquidKid
+    {
+        DiscordClient discord;
+
+
+        public SquidKid()
+        {
+            discord = new DiscordClient(x =>
+            {
+                x.LogLevel = LogSeverity.Info;
+                x.LogHandler = Log;
+            });
+
+            discord.UsingCommands(x =>
+            {
+                x.PrefixChar = '~';
+                x.AllowMentionPrefix = true;
+            });
+
+            var commands = discord.GetService<CommandService>();
+
+            commands.CreateCommand("Status")
+                .Do(async (e) =>
+                {
+                    await e.Channel.SendMessage("Fuck You");
+                });
+
+            discord.ExecuteAndWait(async () =>
+            {
+                await discord.Connect("MzI5NzMwOTI0NTM3MzE1MzI5.DDfGWg.tqJUDn27CADP9err_zXZq81KJlI", TokenType.Bot);
+            });
+        }
+
+        private void Log(object sender, LogMessageEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
