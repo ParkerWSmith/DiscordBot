7b191c0ce0a9a16440eafe1eb06ceaf837fc94c6
ParkerWSmith <smithparker0@gmail.com>
2017-07-06 16:02:52 -0500

    added feature

--- DiscordApp_v.02/SquidKid.cs
+++ DiscordApp_v.02/SquidKid.cs
@@ -12,10 +12,33 @@ namespace DiscordApp_v._02
     class SquidKid
     {
         DiscordClient discord;
+        CommandService commands;
 
+        Random rand;
+
+        string[] henReader;
 
         public SquidKid()
         {
+            rand = new Random();
+
+            henReader = new string[]
+            {
+                "randumb/hr2.jpg",
+                "randumb/hr3.jpg",
+                "randumb/hr4.png",
+                "randumb/hr5.png",
+                "randumb/hr6.jpg",
+                "randumb/hr7.jpg",
+                "randumb/hr8.jpg",
+                "randumb/hr9.jpg",
+                "randumb/hr10.jpg",
+                "randumb/hr11.jpg",
+                "randumb/hr12.jpg",
+                "randumb/hr13.jpg",
+                "randumb/hr14.jpg"
+            };
+
             discord = new DiscordClient(x =>
             {
                 x.LogLevel = LogSeverity.Info;
@@ -28,7 +51,8 @@ namespace DiscordApp_v._02
                 x.AllowMentionPrefix = true;
             });
 
-            var commands = discord.GetService<CommandService>();
+
+            commands = discord.GetService<CommandService>();
 
             commands.CreateCommand("Hello")
                 .Do(async (e) =>
@@ -48,11 +72,26 @@ namespace DiscordApp_v._02
                    await e.Channel.SendMessage("I will go by SquidKid when I am up and running.");
                });
 
-            discord.ExecuteAndWait(async () =>
+            commands.CreateCommand("kiss")
+               .Do(async (e) =>
+               {
+                   await e.Channel.SendMessage("Kyaaaaa~");
+               });
+         
+            commands.CreateCommand("hr")
+                        .Do(async (e) =>
+                        {
+                            int randomHrIndex = rand.Next(henReader.Length);
+                            string hrToPost = henReader[randomHrIndex];
+                            await e.Channel.SendFile(hrToPost);
+                        });
+
+                discord.ExecuteAndWait(async () =>
             {
                 await discord.Connect("MzI5NzMwOTI0NTM3MzE1MzI5.DDfGWg.tqJUDn27CADP9err_zXZq81KJlI", TokenType.Bot);
             });
-        }
+        
+    }
 
         private void Log(object sender, LogMessageEventArgs e)
         {
