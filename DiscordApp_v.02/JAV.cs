ba8b69f02ff4730d08e0f89459741f52f9f3d417
smithparker0 <smithparker0@gmail.com>
2019-05-06 06:55:29 -0500

    Added new commands

---
+++ DiscordApp_v.02/JAV.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public class JAV
+{
+    public DmmParsingProfile(boolean doGoogleTranslation)
+    {
+        super();
+        this.doGoogleTranslation = doGoogleTranslation;
+        if (this.doGoogleTranslation == false)
+            setScrapingLanguage(Language.JAPANESE);
+        scrapeTrailers = true;
+    }
+
+    public DmmParsingProfile(boolean doGoogleTranslation, boolean scrapeTrailers)
+    {
+        super();
+        this.doGoogleTranslation = doGoogleTranslation;
+        if (this.doGoogleTranslation == false)
+            setScrapingLanguage(Language.JAPANESE);
+        this.scrapeTrailers = scrapeTrailers;
+    }
+
+    public DmmParsingProfile(Document document, boolean doGoogleTranslation)
+    {
+        super(document);
+        this.doGoogleTranslation = doGoogleTranslation;
+        if (this.doGoogleTranslation == false)
+            setScrapingLanguage(Language.JAPANESE);
+    }
+    public Title scrapeTitle()
+        {
+            Element titleElement = document.select("[property=og:title]").first();
+            // run a google translate on the japanese title
+            if (doGoogleTranslation)
+            {
+                return new Title(TranslateString.translateStringJapaneseToEnglish(titleElement.attr("content").toString()));
+            }
+            else
+            {
+                return new Title(titleElement.attr("content").toString());
+            }
+        }
+}
